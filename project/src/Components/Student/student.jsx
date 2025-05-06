5aa33aab946defe3b8a67c7f84befcfc9dadab01
Raheel <Raheelali.bscsf22@iba-suk.edu.pk>
2025-05-06 16:05:40 +0500

    2nd commit

---
+++ project/src/Components/Student/student.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const student = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Hello world from student!</h1>
+    </div>
+  );
+};
+
+export default student;
---
+++ project/src/Components/registration/register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const register = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Hello world from register!</h1>
+    </div>
+  );
+};
+
+export default register;
